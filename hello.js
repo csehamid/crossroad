9d195a525701e8a86bc35cff22f207e855a1b06b
Hamid Shekarforoush <csehamid@yahoo.com>
2017-07-22 23:29:14 -0400

    Update hello.js

--- hello.js
+++ hello.js
@@ -1,16 +1,12 @@
-<!DOCTYPE HTML>
-<html>
-
-<body>
-
-  <p>Before the script...</p>
-
-  <script>
-    alert( 'Hello, world!' );
-  </script>
-
-  <p>...After the script.</p>
-
-</body>
-
+<!DOCTYPE html>
+<html lang="en">
+  <head>
+    <title>Some Page</title>
+    <script type="text/javascript">
+      alert("Hello World!");
+    </script>
+  </head>
+  <body>
+    <p>The content of the web page.</p>
+  </body>
 </html>
